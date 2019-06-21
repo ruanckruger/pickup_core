9081dc79f5eeaa41488101eef770f45d76279830
ruank <ruank@velocitycubed.co.za>
2019-06-21 10:06:24 +0200

    Fixes that should h ave it all working

--- pickupsv2/Models/PlayerModel.cs
+++ pickupsv2/Models/PlayerModel.cs
@@ -12,7 +12,6 @@ namespace pickupsv2.Models
         public string steamId{ get; set; }
         public string steamUsername{ get; set; }
         public string name{ get; set; }
-        public string surname{ get; set; }
         public string steaumUrl{ get; set; }
         public string avatar{ get; set; }
         public Guid? curMatch{ get; set; }
