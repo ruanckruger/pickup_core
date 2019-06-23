c184551d5ade1bda5dec5ee47a7614d60496b107
ruank <ruank@velocitycubed.co.za>
2019-06-23 16:35:04 +0200

    10 playerness

--- pickupsv2/Hubs/PickupHub.cs
+++ pickupsv2/Hubs/PickupHub.cs
@@ -52,7 +52,7 @@ namespace pickupsv2.Hubs
                 await db.SaveChangesAsync();
                 var newPlayerCount = db.Players.Where(p => p.curMatch == matchId).Count();
                 await Clients.All.SendAsync("UserJoined", matchId, player.Id, newPlayerCount);
-                if (newPlayerCount == 2)
+                if (newPlayerCount == 10)
                 {
                     await GameReady(matchId);
                 }
